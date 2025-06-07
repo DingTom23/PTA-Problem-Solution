17dfc8e67d7b4bc77f034bb971a9c339294b2720
DingTom <dingtom23@163.com>
2025-06-07 15:23:13 +0800

    update
    update

--- someexamples/upper-to-lower/main.cpp
+++ someexamples/upper-to-lower/main.cpp
@@ -13,13 +13,28 @@ int main() {
     transform(str.begin(),str.end(),str.begin(),::toupper);
     cout<<"转化为大写后为："<<str<<endl;
 
-    // 或者
+    // 或者手动实现
+    cout << "请再输入一个字符串进行手动转换：";
+    string str2;
+    cin >> str2;
 
-    for (int i = 0; i < str.size(); i++){
-        if (str[i] >= 'a' && str[i] <= 'z' || str[i] >= 'A' && str[i] <= 'Z'){
-            
+    // 转换为小写
+    string lower_str = str2;
+    for (int i = 0; i < lower_str.size(); i++){
+        if (lower_str[i] >= 'A' && lower_str[i] <= 'Z'){
+            lower_str[i] = lower_str[i] + 32;  // 大写转小写
         }
     }
+    cout << "手动转换为小写：" << lower_str << endl;
+
+    // 转换为大写
+    string upper_str = str2;
+    for (int i = 0; i < upper_str.size(); i++){
+        if (upper_str[i] >= 'a' && upper_str[i] <= 'z'){
+            upper_str[i] = upper_str[i] - 32;  // 小写转大写
+        }
+    }
+    cout << "手动转换为大写：" << upper_str << endl;
 
     return 0;
 }
